d460d87a98cf1295b50bc3dcd38b0f161b3a0e19
nalbam <me@nalbam.com>
2018-06-12 20:15:37 +0900

    init

---
+++ main.tf
@@ -0,0 +1,18 @@
+# Terraform Main
+
+module "domain" {
+  source = "git::https://github.com/nalbam/terraform-aws-route53.git"
+  domain = "${var.domain}"
+}
+
+module "demo-web" {
+  source = "git::https://github.com/nalbam/terraform-aws-static-web.git"
+  region = "${var.region}"
+
+  zone_id = "${module.domain.zone_id}"
+  certificate_arn = "${module.domain.certificate_arn}"
+
+  domain_name = [
+    "${var.name}.${var.domain}"
+  ]
+}
